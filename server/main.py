1ccb64184ebdffec125804ffe81be1803482ddf4
mahedjaved <mahed95@gmail.com>
2026-05-23 14:11:00 +0100

    [updates:main] adds the routes in the main.py file

--- server/main.py
+++ server/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+
+from routes.upload_pdfs import router as upload_router
+from routes.ask_question import router as ask_router
+
 from fastapi.middleware.cors import CORSMiddleware
 from server.middlewares.exceptionHandlers import catch_exception_from_middleware
 
@@ -19,4 +23,6 @@ app.add_middleware(
 # add middleware exception handlers
 app.middleware("http")(catch_exception_from_middleware)
 
-# add routers - 1) upload PDF documents 2) asing query 
+# add routers - 1) upload PDF documents 2) asing query
+app.include_router(upload_router)
+app.include_router(ask_router)
